143e21e4625e00210703711e41678ec8e40ba22c
fengyuww <2439825946@qq.com>
2022-08-16 15:24:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# autoPrint
+
+If you plan to encrypting or decrypting PDFs that use AES, you will need to install some extra
+dependencies. Encryption using RC4 is supported using the regular installation.
+
+``` 
+pip install PyPDF2[crypto]
+```
--- app.py
+++ app.py
@@ -1,11 +1,232 @@
-from flask import Flask
+import os
+
+import pythoncom
+import win32print
+import win32com.client
+from flask import Flask, request
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
+from PyPDF2 import PdfReader
+from pypinyin import lazy_pinyin
 
 app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False  # 支持中文
+app.config['UPLOAD_FOLDER'] = os.path.abspath('static/files')
+CORS(app, supports_credentials=True)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+class UploadFile(object):
+    def __init__(self, f: FileStorage):
+        self.fileStorage = f
+        self.originFilename = f.filename
+        self.secureFilename = secure_filename(''.join(lazy_pinyin(f.filename)))
+        self.uniqueFilename = UploadFile.make_filename_unique(self.secureFilename)
+        self.extension = str(os.path.splitext(self.secureFilename)[-1]).lower()
+
+    @staticmethod
+    def make_filename_unique(filename: str) -> str:
+        from uuid import uuid4
+        ident = uuid4().__str__()[:8]
+        return f'{ident}_{filename}'
+
+    def save(self):
+        self.fileStorage.save(os.path.join(app.config['UPLOAD_FOLDER'], self.uniqueFilename))
+
+
+class Document(object):
+    def __init__(self, arg: UploadFile | str):
+        """
+        :param arg: UploadFile Object or filename
+        """
+        self.filename = None
+        self.extension = None
+        self.absPath = None
+        self._pages = None
+        self._size = None
+        if isinstance(arg, UploadFile):
+            self.filename = arg.uniqueFilename
+            self.extension = arg.extension
+        elif isinstance(arg, str):
+            self.filename = arg
+            self.extension = str(os.path.splitext(arg)[-1]).lower()
+        else:
+            raise ValueError('Must be UploadFile Object or filename')
+
+        self.absPath = os.path.join(app.config['UPLOAD_FOLDER'], self.filename)
+
+    @property
+    def pages(self) -> int:
+        if self._pages is None:
+            self._pages = ...
+        return self._pages
+
+    @property
+    def size(self) -> str:
+        if self._size is None:
+            st_size = os.stat(self.absPath).st_size
+            if st_size < 1048576:
+                self._size = str(round(st_size / 1024, 2)) + 'KB'
+            else:
+                self._size = str(round(st_size / 1048576, 2)) + 'MB'
+        return self._size
+
+    def convert2pdf(self) -> str:
+        if self.extension == '.pdf':
+            return self.filename
+        if self.extension in ['.doc', '.docx']:
+            word_path = self.absPath
+            pdf_path = os.path.splitext(self.absPath)[0] + '.pdf'
+
+            # Initialize
+            pythoncom.CoInitialize()
+            word = win32com.client.DispatchEx('Word.Application')
+            doc = word.Documents.Open(word_path)
+            doc.SaveAs(pdf_path, FileFormat=17)
+            doc.Close()
+            word.Quit()
+            # Uninitialize
+            pythoncom.CoUninitialize()
+
+        return os.path.splitext(self.filename)[0] + '.pdf'
+
+
+class PDFDocument(Document):
+    def __init__(self, arg: UploadFile | str):
+        super().__init__(arg)
+
+    @property
+    def pages(self) -> int:
+        if self._pages is None:
+            reader = PdfReader(self.absPath)
+            if reader.is_encrypted:
+                reader.decrypt('')
+            self._pages = len(reader.pages)
+        return self._pages
+
+    def dprint(self, options: dict = None):
+        args = ''
+        if options is None:
+            args += '-print-to-default -silent '
+        else:
+            if 'printer' in options:
+                args += '-print-to ' + options['printer'] + ' '
+            else:
+                args += '-print-to-default '
+            args += '-silent '
+            keys = list(options.keys())
+            if 'printer' in keys:
+                keys.remove('printer')
+            if len(keys) > 0:
+                args += '-print-settings '
+            if 'pages' in options:
+                args += options['pages'] + ','
+            if 'monochrome' in options and options['monochrome']:
+                args += 'monochrome,'
+            if 'side' in options:
+                args += options['side'] + ','
+            if 'paperSize' in options:
+                args += 'paper=' + options['paperSize'] + ','
+            if 'copies' in options:
+                args += str(options['copies']) + 'x,'
+
+        cmd = os.path.abspath('SumatraPDF.exe') + ' ' + args[:-1] + ' ' + self.absPath
+        os.system(cmd)
+
+
+class PrinterUtil(object):
+    @staticmethod
+    def trans_job_info(job_info):
+        ret = {
+            'jobId': job_info['JobId'],
+            'printerName': job_info['pPrinterName'],
+            'document': job_info['pDocument'],
+            'status': job_info['Status'],
+            'priority': job_info['Priority'],
+            'position': job_info['Position'],
+            'totalPages': job_info['TotalPages'],
+            'pagesPrinted': job_info['PagesPrinted']
+        }
+        return ret
+
+    @staticmethod
+    def get_default_printer() -> str:
+        return win32print.GetDefaultPrinter()
+
+    @staticmethod
+    def set_default_printer(printer: str):
+        win32print.SetDefaultPrinter(printer)
+
+    @staticmethod
+    def enum_jobs(printer: str):
+        handle = win32print.OpenPrinter(printer)
+        jobs_info = win32print.EnumJobs(handle, 0, -1)
+        win32print.ClosePrinter(handle)
+        data = []
+        for job in jobs_info:
+            data.append(PrinterUtil.trans_job_info(job))
+        return data
+
+    @staticmethod
+    def get_job(printer: str, job_id: int):
+        handle = win32print.OpenPrinter(printer)
+        job_info = win32print.GetJob(handle, job_id)
+        win32print.ClosePrinter(handle)
+        return PrinterUtil.trans_job_info(job_info)
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        upload_file = UploadFile(f)
+        upload_file.save()
+        doc = Document(upload_file)
+        pdf = PDFDocument(doc.convert2pdf())
+        return {
+            'id': pdf.filename.split('_')[0],
+            'originFilename': upload_file.originFilename,
+            'newFilename': pdf.filename,
+            'size': pdf.size,
+            'pages': pdf.pages,
+            'extension': upload_file.extension
+        }
+    else:
+        return 'method not allowed'
+
+
+@app.route('/get_default_printer')
+def get_default_printer():
+    return PrinterUtil.get_default_printer()
+
+
+@app.route('/enum_jobs')
+def enum_jobs():
+    data = PrinterUtil.enum_jobs(PrinterUtil.get_default_printer())
+    return {
+        'data': data
+    }
+
+
+@app.route('/get_job')
+def get_job():
+    job_id = int(request.args.get('jobID'))
+    return PrinterUtil.get_job(PrinterUtil.get_default_printer(), job_id)
+
+
+@app.route('/print', methods=['post'])
+def print_document():
+    data = request.get_json()
+    pdf = PDFDocument(data['filename'])
+    pdf.dprint(data['options'])
+    jobs = PrinterUtil.enum_jobs(PrinterUtil.get_default_printer())
+    job_id = None
+    for job in jobs:
+        if os.path.basename(job['document']) == pdf.filename:
+            job_id = job['jobId']
+    return {
+        'jobID': job_id
+    }
 
 
 if __name__ == '__main__':
